088b2f26ff5172394e718c3103e1a80400448df0
Jaime <jaimesanchezgalvis@gmail.com>
2020-09-23 15:37:17 +0200

    UsersNavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Routes from "./Routes";
 import './App.css';
 import LoginViewPage from './pages/sharedpages/loginregisterpages/pages/LoginViewPage/LoginViewPage';
+import {UsersNavBar} from "./pages/userpages/shared/UsersNavBar/UsersNavBar";
+
 
 
 function App() {
   return (
       <div>
         <Routes/>
+          <UsersNavBar UsersNavBar={UsersNavBar}/>
       </div>
   );
 }
---
+++ src/pages/userpages/shared/UsersNavBar/UsersNavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./UsersNavBar.scss";
+
+export function UsersNavBar() {
+    return (
+
+        <footer className="UsersNavBar">
+            <img src="./inicio@2x.png" alt="icono"/>
+            <img src="./buscar@2x.png" alt="icono"/>
+            <img src="./mensaje@2x.png" alt="icono"/>
+            <img src="./usuario@2x.png" alt="icono"/>
+        </footer>
+
+    );
+}
+
