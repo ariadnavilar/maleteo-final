37cb4c4bf9c29c4d8479db9553e5c38ffb42a2ea
ariadnavilar <ariadna.vilar46@gmail.com>
2020-09-25 17:00:15 +0200

    dev actualizada mapa

--- src/pages/userpages/FindYourGuardianPage/components/Form/Form.jsx
+++ src/pages/userpages/FindYourGuardianPage/components/Form/Form.jsx
@@ -40,10 +40,10 @@ export default function Form() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="centered">
                     <span className="pi pi-search formicons"></span>
-                    <input className="controls" type="text" name="location" id="location"
+                    <input className="inputsearch controls" type="text" name="location" id="location"
                            placeholder="¿Dónde te encuentras? Madrid, Barcelona..." ref={register({required: true})}/>
                 </div>
-                {/* <div className="row centered">
+                <div className="row centered">
                     <input className="datesearch calendar" type="date" name="deposit" placeholder="Depósito"
                            ref={register({required: true})}/>
                     <input className="datesearch calendar" type="date" name="withdrawal" placeholder="Retirada"
@@ -53,10 +53,8 @@ export default function Form() {
                     <span className="pi pi-briefcase luggageicon"></span>
                     <input className="datesearch" type="number" name="luggage" placeholder="Nº de piezas"
                            ref={register({required: true})}/>
-
-
-                </div> */}
                     <input className="orangebtn sm" type="submit" value="Buscar"/>
+                </div>
                 {errors.location && errors.withdrawal && errors.deposit && errors.luggage &&
                 <span className="warning">Introduce todos los campos necesarios</span>}
             </form>
