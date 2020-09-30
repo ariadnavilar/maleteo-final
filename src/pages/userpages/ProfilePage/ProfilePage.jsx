e63cc37e006c6d9ac49fca9ded678bb210ca329a
Ariadna Vilar <68858092+ariadnavilar@users.noreply.github.com>
2020-09-30 16:06:21 +0200

    Update ProfilePage.jsx

--- src/pages/userpages/ProfilePage/ProfilePage.jsx
+++ src/pages/userpages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {API} from "../../../shared/services/api";
+import { API } from "../../../shared/services/api";
 import {Link, useHistory} from "react-router-dom";
 import "./ProfilePage.scss";
 import Modal from 'react-modal';
@@ -26,52 +26,39 @@ export default function ProfilePage(props) {
     const editProfile = () =>
         history.push('/profile/edit');
 
-
     useEffect(() => {
         API.get('bookings/' + email).then(res =>
-            setBookings(res.data)
-        )
-    }, [])
-    console.log(bookings);
+        setBookings(res.data)
+    )}, [])
+
     return (
         <div className="margintop">
             <h1>¡Hola {user.name}!</h1>
-
-            <button className="whitebtn sm" onClick={() => setModalIsOpen(true)}><span
-                className="pi pi-calendar"></span> Reservas
-            </button>
-
             <div>
-                <button className="whitebtn sm" onClick={() => setModalIsOpen(true)}><span
-                    className="pi pi-calendar orangeicons"></span> Tus reservas
-                </button>
+                <button className="whitebtn sm" onClick={()=>setModalIsOpen(true)}><span className="pi pi-calendar orangeicons"></span> Tus reservas</button>
             </div>
             <Modal isOpen={modalIsOpen}>
                 <div className="closewindow">
-                    <button className="closebtn" onClick={() => setModalIsOpen(false)}><span
-                        className="pi pi-times completed"></span></button>
+                    <button className="closebtn" onClick={()=>setModalIsOpen(false)}><span className="pi pi-times completed"></span></button>
                 </div>
                 <h3>Tus reservas</h3>
                 {bookings && <div>
                     {bookings.map((booking, i) =>
                         <div>
-                            <h5>Reserva nº{1 + i++}</h5>
+                            <h5>Reserva nº{1+i++}</h5>
                             <p>{booking.initialDate}</p>
                             <p>{booking.finalDate}</p>
-                            <Link to={"/bookings/:id" + booking._id}>Detalles de la reserva</Link>
+                            <p>Tu guardián es {booking.guardian.name}</p>
+                            <Link to="/bookings/:id">Detalles de la reserva</Link>
                         </div>
-                    )}
+                        )}
                 </div>}
             </Modal>
             <div>
-                <button className="whitebtn sm" onClick={guardianRegister}><span
-                    className="pi pi-star-o orangeicons"></span> Registrarse como guardián
-                </button>
+                <button className="whitebtn sm" onClick={guardianRegister}><span className="pi pi-star-o orangeicons"></span> Registrarse como guardián</button>
             </div>
             <div>
-                <button className="whitebtn sm" onClick={editProfile}><span
-                    className="pi pi-user-edit orangeicons"></span> Editar perfil
-                </button>
+                <button className="whitebtn sm" onClick={editProfile}><span className="pi pi-user-edit orangeicons"></span> Editar perfil</button>
             </div>
             <div className="centered">
                 <button className="orangebtn endbtn" onClick={signOut}>Cerrar sesión</button>
