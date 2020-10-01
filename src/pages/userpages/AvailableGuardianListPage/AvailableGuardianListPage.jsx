f73f1c68a37364b6890a40f38f083545f8677f33
Eduardo Rodríguez <eduardostifler5@gmail.com>
2020-10-01 17:12:09 +0200

    Nuevo carousel conectado con el mapa

--- src/pages/userpages/AvailableGuardianListPage/AvailableGuardianListPage.jsx
+++ src/pages/userpages/AvailableGuardianListPage/AvailableGuardianListPage.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useCallback} from "react";
 import Location from "./components/Location/Location";
 import { API} from "../../../shared/services/api";
-import { Carousel } from "primereact/carousel";
+import Carousel from 'react-bootstrap/Carousel'
 import { Link} from "react-router-dom";
 import "./AvailableGuardianListPage.scss";
 import {UsersNavBar} from "../shared/UsersNavBar/UsersNavBar";
@@ -19,31 +19,37 @@ export default function AvailableGuardianListPage() {
         )},
          [])
 
-    const itemTemplate = (guardian) =>{
-        return  (
-            <div className="p-carousel-content guardianlist">
-                <Link to={"/selectedguardian/" + guardian._id}>
-                    <div className="row guardianslide">
-                        <div className="inlinediv col-6">
-                            <img className="homeimg" src={guardian.images[0]}/>
-                        </div>
-                        <div className="inlindediv col-6">
-                            <p className="textinfo">{guardian.name} {guardian.surname}</p>
-                            <img className="personalimg" src={guardian.personalImage}/>
-                            <p className="textinfo locationtext"><span className="pi pi-map-marker"></span>
-                                {guardian.location}
-                            </p>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-        )
-    }
-console.log(activeGuardian)
+    const handleSelect = (selectedIndex, e) => {
+            setActiveGuardian(selectedIndex);
+          };
+
     return (
                     <div>
                         <Location guardians={guardians} setActiveGuardian={setActiveGuardian}/>
-                        <Carousel value={guardians} itemTemplate={itemTemplate} page={activeGuardian} onPageChange={(e) => setActiveGuardian(e.page)}></Carousel>
+                        <Carousel activeIndex={activeGuardian} onSelect={handleSelect} interval={500000000}>
+                                {guardians.map((guardian,i)=> 
+
+                                    <Carousel.Item>
+                                    <div className="p-carousel-content guardianlist">
+                                        <Link to={"/selectedguardian/" + guardian._id}>
+                                            <div className="row guardianslide">
+                                                <div className="inlinediv col-6">
+                                                    <img className="homeimg" src={guardian.images[0]}/>
+                                                </div>
+                                                <div className="inlindediv col-6">
+                                                    <p className="textinfo">{guardian.name} {guardian.surname}</p>
+                                                    <img className="personalimg" src={guardian.personalImage}/>
+                                                    <p className="textinfo locationtext"><span className="pi pi-map-marker"></span>
+                                                        {guardian.location}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    </Carousel.Item>
+                                )}
+
+                        </Carousel>
                         <UsersNavBar/>
                     </div>
     )
